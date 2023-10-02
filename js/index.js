2bf76e5f63316ba25de97e361a3ced477d8de67e
Lima-49 <vitorlima.soares@hotmail.com>
2023-10-02 16:30:14 -0300

    criacao de um php para conectar com o banco

--- js/index.js
+++ js/index.js
@@ -1,110 +1,76 @@
-const apiURL = "https://api-check-domain.onrender.com/api/verificar-dominio/"
+const apiURL = "https://api-check-domain.onrender.com/api/verificar-dominio/";
 
-// Adicione um evento de clique ao botão "Verificar Domínio"
-document.getElementById('search_btn').addEventListener('click', async function () {
-    
-    // Recupere o valor do campo de entrada
-    var dominio = document.getElementById('search_input').value;
+const searchBtn = document.getElementById('search_btn');
+const searchInput = document.getElementById('search_input');
+const loadingContainer = document.querySelector('.loading_conteiner');
 
-    document.querySelector('.loading_conteiner').style.display = 'block';
+searchBtn.addEventListener('click', async function () {
+    var domainSearched = await searchDomain();
+    //window.location.href = domainSearched
+});
 
-    // Verifique se o campo de entrada não está vazio
-    if (dominio.trim() !== '') {
+searchInput.addEventListener('keyup', async function (event) {
+    if (event.key === 'Enter') {
+        var domainSearched = await searchDomain();
+        //window.location.href = domainSearched
+    }
+});
+
+async function searchDomain() {
+    const dominio = searchInput.value.trim();
+    loadingContainer.style.display = 'block';
+
+    if (dominio !== '') {
         try {
-            // Faça a chamada de API usando o valor do domínio
-            let headersList = {
+            const headersList = {
                 "Accept": "*/*",
                 "User-Agent": "Thunder Client (https://www.thunderclient.com)"
             };
 
-            console.log(`${apiURL}${dominio}`)
-
-            let response = await fetch(`${apiURL}${dominio}`, {
+            const response = await fetch(`${apiURL}${dominio}`, {
                 method: "GET",
                 headers: headersList,
-                timeout: 20000 // Defina um timeout para a solicitação (10 segundos)
+                timeout: 10000
             });
 
             if (response.status === 200) {
-                // A resposta foi bem-sucedida, então obtenha os dados
-                let data = await response.text();
+                const data = await response.text();
                 localStorage.setItem('dominio', dominio);
-                localStorage.setItem('data', data)
-                window.location.href = `info.html?domain=${dominio}`;
+                localStorage.setItem('data', data);
 
-                document.querySelector('.loading_conteiner').style.display = 'none'
+                await insertDataBase(dominio);
+                return `info.html?domain=${dominio}`;
 
             } else {
-                // A resposta não foi bem-sucedida, trate o erro aqui
                 console.error('Erro na chamada da API');
-                document.querySelector('.loading_conteiner').style.display = 'none';
-
             }
         } catch (error) {
             console.error('Erro na chamada da API', error);
-            document.querySelector('.loading_conteiner').style.display = 'none';
-
+        } finally {
+            loadingContainer.style.display = 'none';
         }
     } else {
-        // O campo de entrada está vazio, trate isso aqui
         console.error('Campo de entrada vazio');
-        document.querySelector('.loading_conteiner').style.display = 'none';
-
+        loadingContainer.style.display = 'none';
     }
-});
+}
 
+async function insertDataBase(dominio) {
+    try {
 
-// Adicione um evento de clique ao botão "Verificar Domínio"
-document.getElementById('search_input').addEventListener('keyup', async function (event) {
+        const response = await fetch("./php/database.php", {
+            method: "POST",
+            body: `dominio_pesquisa=${dominio}`,
+            timeout: 10000
+        });
 
-    if (event.key == 'Enter'){
-
-        document.querySelector('.loading_conteiner').style.display = 'block';
-        
-        // Recupere o valor do campo de entrada
-        var dominio = document.getElementById('search_input').value;
-
-        // Verifique se o campo de entrada não está vazio
-        if (dominio.trim() !== '') {
-            try {
-                // Faça a chamada de API usando o valor do domínio
-                let headersList = {
-                    "Accept": "*/*",
-                    "User-Agent": "Thunder Client (https://www.thunderclient.com)"
-                };
-                
-                console.log(`${apiURL}${dominio}`)
-
-                let response = await fetch(`${apiURL}${dominio}`, {
-                    method: "GET",
-                    headers: headersList,
-                    timeout: 10000 // Defina um timeout para a solicitação (10 segundos)
-                });
-
-                if (response.status === 200) {
-                    // A resposta foi bem-sucedida, então obtenha os dados
-                    let data = await response.text();
-                    localStorage.setItem('dominio', dominio);
-                    localStorage.setItem('data', data)
-                    window.location.href = `info.html?domain=${dominio}`;
-
-                    document.querySelector('.loading_conteiner').style.display = 'none';
-
-                } else {
-                    // A resposta não foi bem-sucedida, trate o erro aqui
-                    console.error('Erro na chamada da API');
-                    document.querySelector('.loading_conteiner').style.display = 'none';
-                }
-            } catch (error) {
-                console.error('Erro na chamada da API', error);
-                document.querySelector('.loading_conteiner').style.display = 'none';
-            }
+        if (response.status === 200) {
+            const data = await response.text();
+            console.log("Resposta do servidor: ", data);
         } else {
-            // O campo de entrada está vazio, trate isso aqui
-            console.error('Campo de entrada vazio');
-            document.querySelector('.loading_conteiner').style.display = 'none';
+            console.error('Erro na chamada da API');
         }
+    } catch (error) {
+        console.error('Erro na chamada da API', error);
     }
-    
-});
-
+}
--- js/info.js
+++ js/info.js
@@ -1,217 +1,162 @@
-// Recupere o valor do domínio da URL
 const urlParams = new URLSearchParams(window.location.search);
+const dominio = localStorage.getItem('dominio');
+const data = JSON.parse(localStorage.getItem('data'));
 
-// Recupere o valor do domínio do Local Storage
-var dominio = localStorage.getItem('dominio');
-var data = JSON.parse(localStorage.getItem('data'));
-
-// Verifique se o valor do domínio não está vazio
 if (dominio) {
-    // Atualize o conteúdo do elemento HTML com o valor do domínio
     document.getElementById('domain-label').textContent = dominio;
     document.getElementById('dashboard-title').textContent = dominio;
 }
 
-document.getElementById('back-button').addEventListener('click', async function () {
-    window.location.href = "index.html"
+document.getElementById('back-button').addEventListener('click', function () {
+    window.location.href = "index.html";
 });
 
-document.getElementById('logo_home').addEventListener('click', async function () {
-  window.location.href = "index.html"
+document.getElementById('logo_home').addEventListener('click', function () {
+    window.location.href = "index.html";
 });
 
-
-// Selecione a div com a classe "StatusSummary_row"
 const statusRow = document.querySelector('.StatusSummary_row');
-
-// Acesse o valor de data[dmarc][info][exists]
 const dmarcExists = data.dmarc.info.exists;
-
-// Acesse o valor de data[dmarc][info][at_enforcement]
 const dmarcAtEnforcement = data.dmarc.info.at_enforcement;
-
-// Acesse o valor de data[dmarc][info][text]
 const dmarcText = data.dmarc.info.text;
-
-// Selecione a tag <img> com o ID "status-icon"
 const statusIcon = document.getElementById('status-icon');
-
-// Selecione a tag <img> com o ID "list_icon"
 const listIcon = document.getElementById('list_icon');
-
-if (dmarcExists){
-
-    if (dmarcAtEnforcement) {
-      // Se for verdadeiro, aplique a classe com o fundo verde
-      statusRow.classList.add('background-green');
-      document.getElementById('status_summary_title').textContent = "Protegido";
-      document.getElementById('status_summary_sub_title').textContent = "Contra ataques de falsificação de identidade";
-      statusIcon.src = "./img/email_correto_branco.png";
-
-      listIcon.src = "./img/email_correto_branco.png";
-      const dmarcList = document.getElementById('dmarc-list');
-      const novoItem = document.createElement('li')
-      novoItem.textContent = "Muito bem! Seu domínio está protegido contra abusos por phishers e spammers e atende às nossas recomendações de segurança.";
-      novoItem.classList.add('List_item')
-      dmarcList.appendChild(novoItem);
-
-    } else {
-      // Caso contrário, aplique a classe com o fundo vermelho
-      statusRow.classList.add('background-red');
-      document.getElementById('status_summary_title').textContent = "Desprotegido";
-      document.getElementById('status_summary_sub_title').textContent = "Contra ataques de falsificação de identidade";
-
-      document.querySelector('#btn_proteger_dominio').style.display = 'block';
-      
-      statusIcon.src = "./img/email_incorreto_branco.png";
-      listIcon.src = "./img/email_incorreto_branco.png";
-      document.querySelector('#btn_proteger_dominio').style.display = 'block';
-
-      const dmarcList = document.getElementById('dmarc-list');
-      const novoItem = document.createElement('li')
-      novoItem.textContent = "Seu domínio não está protegido contra abuso por phishers e spammers! Qualquer um pode enviar mensagens se passando por esse domínio, sem ser detectado.";
-      novoItem.classList.add('List_item')
-      dmarcList.appendChild(novoItem);
-
-    }
-
-} else{
-  // Caso contrário, aplique a classe com o fundo vermelho
-  statusRow.classList.add('background-red');
-  document.getElementById('status_summary_title').textContent = "Desprotegido";
-  document.getElementById('status_summary_sub_title').textContent = "Contra ataques de falsificação de identidade";
-
-  statusIcon.src = "./img/email_incorreto_branco.png";
-  listIcon.src = "./img/email_incorreto_branco.png";
-  document.querySelector('#btn_proteger_dominio').style.display = 'block';
-
-  const dmarcList = document.getElementById('dmarc-list');
-
-  const itemUm = document.createElement('li')
-  itemUm.textContent = "Seu domínio não está totalmente protegido contra abusos de phishers e spammers. Qualquer um pode enviar mensagens se passando por esse domínio, sem ser detectado.";
-  itemUm.classList.add('List_item')
-
-  const itemDois = document.createElement('li')
-  itemDois.textContent = "Deixe-nos ajudá-lo a corrigir isso e iniciar uma avaliação gratuita de 21 dias."
-  itemDois.classList.add('List_item')
-
-  dmarcList.appendChild(itemUm)
-  dmarcList.appendChild(itemDois)
-
-}
-
-// Encontre o botão por ID
 const btnProtegerDominio = document.getElementById('btn_proteger_dominio');
 
-// Adicione um evento de clique ao botão
-btnProtegerDominio.addEventListener('click', function() {
-    // Redirecione para a URL desejada
+if (dmarcExists) {
+    if (dmarcAtEnforcement) {
+        statusRow.classList.add('background-green');
+        document.getElementById('status_summary_title').textContent = "Protegido";
+        document.getElementById('status_summary_sub_title').textContent = "Contra ataques de falsificação de identidade";
+        statusIcon.src = "./img/email_correto_branco.png";
+        listIcon.src = "./img/email_correto_branco.png";
+
+        const dmarcList = document.getElementById('dmarc-list');
+        const novoItem = document.createElement('li');
+        novoItem.textContent = "Muito bem! Seu domínio está protegido contra abusos por phishers e spammers e atende às nossas recomendações de segurança.";
+        novoItem.classList.add('List_item');
+        dmarcList.appendChild(novoItem);
+    } else {
+        statusRow.classList.add('background-red');
+        document.getElementById('status_summary_title').textContent = "Desprotegido";
+        document.getElementById('status_summary_sub_title').textContent = "Contra ataques de falsificação de identidade";
+        document.querySelector('#btn_proteger_dominio').style.display = 'block';
+        statusIcon.src = "./img/email_incorreto_branco.png";
+        listIcon.src = "./img/email_incorreto_branco.png";
+        document.querySelector('#btn_proteger_dominio').style.display = 'block';
+
+        const dmarcList = document.getElementById('dmarc-list');
+        const novoItem = document.createElement('li');
+        novoItem.textContent = "Seu domínio não está protegido contra abuso por phishers e spammers! Qualquer um pode enviar mensagens se passando por esse domínio, sem ser detectado.";
+        novoItem.classList.add('List_item');
+        dmarcList.appendChild(novoItem);
+    }
+} else {
+    statusRow.classList.add('background-red');
+    document.getElementById('status_summary_title').textContent = "Desprotegido";
+    document.getElementById('status_summary_sub_title').textContent = "Contra ataques de falsificação de identidade";
+    statusIcon.src = "./img/email_incorreto_branco.png";
+    listIcon.src = "./img/email_incorreto_branco.png";
+    document.querySelector('#btn_proteger_dominio').style.display = 'block';
+
+    const dmarcList = document.getElementById('dmarc-list');
+    const itemUm = document.createElement('li');
+    itemUm.textContent = "Seu domínio não está totalmente protegido contra abusos de phishers e spammers. Qualquer um pode enviar mensagens se passando por esse domínio, sem ser detectado.";
+    itemUm.classList.add('List_item');
+
+    const itemDois = document.createElement('li');
+    itemDois.textContent = "Deixe-nos ajudá-lo a corrigir isso e iniciar uma avaliação gratuita de 21 dias.";
+    itemDois.classList.add('List_item');
+
+    dmarcList.appendChild(itemUm);
+    dmarcList.appendChild(itemDois);
+}
+
+btnProtegerDominio.addEventListener('click', function () {
     window.location.href = 'https://mailsecurity.com.br/mailsecurity-dmarc-enforcement/';
 });
 
-if(dmarcText != null){
+if (dmarcText) {
+    const dmarcList = document.getElementById('dmarc-list');
+    const listSubTitle = document.createElement('li');
+    listSubTitle.textContent = "Registro DMARC.";
+    listSubTitle.classList.add('List_sub-title');
+    dmarcList.appendChild(listSubTitle);
 
-  const dmarcList = document.getElementById('dmarc-list');
-  const listSubTitle = document.createElement('li')
-  listSubTitle.textContent = "Registro DMARC."
-  listSubTitle.classList.add('List_sub-title')
-  dmarcList.appendChild(listSubTitle)
+    const novoItem = document.createElement('li');
+    novoItem.className = 'code-block';
 
-  const novoItem = document.createElement('li');
-  novoItem.className = 'code-block';
+    const codeElement = document.createElement('code');
+    codeElement.textContent = dmarcText;
+    novoItem.appendChild(codeElement);
 
-  const codeElement = document.createElement('code');
-  codeElement.textContent = dmarcText;
-  novoItem.appendChild(codeElement);
-
-  dmarcList.appendChild(novoItem);
-
+    dmarcList.appendChild(novoItem);
 }
 
-
-// Acesse o valor de data[spf][info][exists]
 const spfExists = data.spf.info.exists;
-
-// Acesse o valor de data[spf][info][valid]
 const spfAtEnforcement = data.spf.info.valid;
-
-// Acesse o valor de data[spf][info][text]
 const spfText = data.spf.info.text;
-
-// Selecione a tag <img> com o ID "list_icon"
 const listIconSpf = document.getElementById('list_icon_spf');
 
-if (spfExists){
+if (spfExists) {
+    if (spfAtEnforcement) {
+        listIconSpf.src = "./img/email_correto_branco.png";
+        const spfList = document.getElementById('spf-list');
+        const itemUm = document.createElement('li');
+        itemUm.textContent = "Parabéns! Seu registro SPF é válido. Um registo SPF está configurado para este domínio e cumpre as nossas recomendações de boas práticas.";
+        itemUm.classList.add('List_item');
 
-  if (spfAtEnforcement) {
+        const itemDois = document.createElement('li');
+        itemDois.textContent = `Os servidores de E-mail que processam este registo SPF efectuam um máximo de ${data.spf.info.domain_lookup_count} pesquisas de domínio. O SPF limita cada registo a um máximo de 10 pesquisas de domínio.`;
+        itemDois.classList.add('List_item');
 
-    listIconSpf.src = "./img/email_correto_branco.png";
-    const spfList = document.getElementById('spf-list');
-    
-    const itemUm = document.createElement('li')
-    itemUm.textContent = "Parabéns! Seu registro SPF é válido. Um registo SPF está configurado para este domínio e cumpre as nossas recomendações de boas práticas.";
-    itemUm.classList.add('List_item')
-    
-    const itemDois = document.createElement('li')
-    itemDois.textContent = `Os servidores de E-mail que processam este registo SPF efectuam um máximo de ${data.spf.info.domain_lookup_count} pesquisas de domínio. O SPF limita cada registo a um máximo de 10 pesquisas de domínio.`
-    itemDois.classList.add('List_item')
+        spfList.appendChild(itemUm);
+        spfList.appendChild(itemDois);
+    } else {
+        listIconSpf.src = "./img/email_incorreto_branco.png";
+        const spfList = document.getElementById('spf-list');
+        const itemUm = document.createElement('li');
+        itemUm.textContent = "A configuração SPF para este domínio tem erros que afetarão a segurança, a visibilidade e a capacidade de entrega do correio eletrónico enviado a partir do domínio.";
+        itemUm.classList.add('List_item');
 
-    spfList.appendChild(itemUm);
-    spfList.appendChild(itemDois);
+        const itemDois = document.createElement('li');
+        itemDois.textContent = ` Os receptores estão limitados a 10 pesquisas de DNS quando avaliam registos SPF.
+        A avaliação completa deste registo SPF requer ${spf_info.domain_lookup_count} pesquisas.
+        Algumas ou todas as mensagens de correio eletrónico enviadas a partir deste domínio podem não ser autenticadas com êxito.`;
+        itemDois.classList.add('List_item');
 
-  } else {
-
+        spfList.appendChild(itemUm);
+        spfList.appendChild(itemDois);
+    }
+} else {
     listIconSpf.src = "./img/email_incorreto_branco.png";
     const spfList = document.getElementById('spf-list');
-    
-    const itemUm = document.createElement('li')
-    itemUm.textContent = "A configuração SPF para este domínio tem erros que afetarão a segurança, a visibilidade e a capacidade de entrega do correio eletrónico enviado a partir do domínio.";
-    itemUm.classList.add('List_item')
-    
-    const itemDois = document.createElement('li')
-    itemDois.textContent = ` Os receptores estão limitados a 10 pesquisas de DNS quando avaliam registos SPF.
-    A avaliação completa deste registo SPF requer ${spf_info.domain_lookup_count} pesquisas.
-    Algumas ou todas as mensagens de correio eletrónico enviadas a partir deste domínio podem não ser autenticadas com êxito.`
-    itemDois.classList.add('List_item')
+
+    const itemUm = document.createElement('li');
+    itemUm.textContent = "Um registro SPF não foi configurado para esse domínio.";
+    itemUm.classList.add('List_item');
+
+    const itemDois = document.createElement('li');
+    itemDois.textContent = "Emails enviados a partir desse domínio não serão autenticados.";
+    itemDois.classList.add('List_item');
 
     spfList.appendChild(itemUm);
     spfList.appendChild(itemDois);
-
-  }
-
-} else{
-
-listIconSpf.src = "./img/email_incorreto_branco.png";
-const dmarcList = document.getElementById('spf-list');
-
-const itemUm = document.createElement('li')
-itemUm.textContent = "Um registro SPF não foi configurado para esse domínio."
-itemUm.classList.add('List_item')
-
-const itemDois = document.createElement('li')
-itemDois.textContent = "Emails enviados a partir desse domínio não serão authenticados"
-itemDois.classList.add('List_item')
-
-dmarcList.appendChild(itemUm)
-dmarcList.appendChild(itemDois)
-
 }
 
-if(spfText != null){
+if (spfText) {
+    const spfList = document.getElementById('spf-list');
+    const listSubTitle = document.createElement('li');
+    listSubTitle.textContent = "Registro SPF.";
+    listSubTitle.classList.add('List_sub-title');
+    spfList.appendChild(listSubTitle);
 
-  const dmarcList = document.getElementById('spf-list');
-  const listSubTitle = document.createElement('li')
-  listSubTitle.textContent = "Registro SPF."
-  listSubTitle.classList.add('List_sub-title')
-  dmarcList.appendChild(listSubTitle)
+    const novoItem = document.createElement('li');
+    novoItem.className = 'code-block';
 
-  const novoItem = document.createElement('li');
-  novoItem.className = 'code-block';
+    const codeElement = document.createElement('code');
+    codeElement.textContent = spfText;
+    novoItem.appendChild(codeElement);
 
-  const codeElement = document.createElement('code');
-  codeElement.textContent = spfText;
-  novoItem.appendChild(codeElement);
-
-  dmarcList.appendChild(novoItem);
-
+    spfList.appendChild(novoItem);
 }
